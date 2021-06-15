77803af461ded62a292ed4246b68961e29175f55
szymon-drzewiecki <249478@student.pwr.edu.pl>
2021-06-15 20:41:25 +0200

    modyfikacja pliku build.gradle, dodanie klasy obsługującej połączenie z bazą danych :)

---
+++ app/src/main/java/org/pytorch/demo/objectdetection/DatabaseAccess.java
@@ -0,0 +1,33 @@
+package org.pytorch.demo.objectdetection;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseAccess {
+    private SQLiteOpenHelper openHelper;
+    private SQLiteDatabase db;
+    private static DatabaseAccess instance;
+    Cursor cursor = null;
+
+    private DatabaseAccess(Context context){
+        this.openHelper = new DatabaseHelper(context);
+    }
+
+    public static DatabaseAccess getInstance(Context context){
+        if (instance==null){
+            instance = new DatabaseAccess(context);
+        }
+        return instance;
+    }
+
+    public void open() {
+        this.db=openHelper.getWritableDatabase();
+    }
+    public void close() {
+        if (db != null) {
+            this.db.close();
+        }
+    }
+}
